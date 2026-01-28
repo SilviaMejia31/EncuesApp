8af3539c41ace7983d2c07af1d5e32242c93bbc9
SilviaMejia31 <159596649+SilviaMejia31@users.noreply.github.com>
2026-01-27 19:50:25 -0600

    Improve sqlite connection

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,4 +1,8 @@
 import sqlite3
 
+DB_PATH = "database.db"
+
 def get_connection():
-    return sqlite3.connect("database.db")
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
